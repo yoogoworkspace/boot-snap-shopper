28caccdbeb6de364b7fb1680a9ee749d7d15d369
yoogoworkspace <yoogoworkspace@gmail.com>
2025-08-15 00:38:13 +0530

    h

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// Dynamically set basename depending on where the app is served
-const pathName = window.location.pathname;
-const baseName = pathName.startsWith("/bootbucket") ? "/bootbucket" : "/";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter basename={baseName}>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
