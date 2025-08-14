1b3fcfc5510be8f34bd5c4807d5f55c81c57576a
yoogoworkspace <yoogoworkspace@gmail.com>
2025-08-15 00:34:28 +0530

    j

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// The base path should match vite.config.js `base`
+// This ensures routes like /bootbucket/page1 work
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter basename="/bootbucket">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
